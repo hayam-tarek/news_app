bd8324be001a39d900649eab69b703e971a4f6bd
Hayam <hayamtareq28@gmail.com>
2024-08-07 23:15:58 +0300

    add loading indicator to web view

--- lib/pages/web_view_page.dart
+++ lib/pages/web_view_page.dart
@@ -15,11 +15,33 @@ class WebViewPage extends StatefulWidget {
 class _WebViewPageState extends State<WebViewPage> {
   final WebViewController webViewController = WebViewController();
   bool isLoading = true;
+  double progress = 0;
+  String pageTitle = '';
   @override
   void initState() {
     webViewController.setJavaScriptMode(JavaScriptMode.unrestricted);
     webViewController.setBackgroundColor(Colors.white);
     webViewController.loadRequest(Uri.parse(widget.url));
+    webViewController.setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (progress) {
+          setState(() {
+            this.progress = progress / 100;
+          });
+        },
+        onPageStarted: (url) {
+          setState(() {
+            pageTitle = 'Loading...';
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            pageTitle = url;
+            progress = 0;
+          });
+        },
+      ),
+    );
     super.initState();
   }
 
@@ -40,8 +62,25 @@ class _WebViewPageState extends State<WebViewPage> {
           ),
         ),
       ),
-      body: WebViewWidget(
-        controller: webViewController,
+      body: SafeArea(
+        child: Column(
+          children: [
+            if (progress > 0 && progress < 1)
+              LinearProgressIndicator(
+                value: progress,
+                valueColor: const AlwaysStoppedAnimation<Color>(
+                  Colors.amber,
+                ),
+                color: Colors.amber,
+                backgroundColor: Colors.amber.withOpacity(.4),
+              ),
+            Expanded(
+              child: WebViewWidget(
+                controller: webViewController,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
